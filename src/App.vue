162f71a506c593565cb9101fa2b947d4d98790b0
pawel.szczepankiewicz <pawel.szczepankiewicz@enginiety.com>
2021-10-12 13:23:16 +0200

    Project start

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learing-resources/StoredResources.vue'
+
+export default {
+  components: { StoredResources },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/learing-resources/StoredResources.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul>
+    <learning-resource
+      v-for="res in resources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue'
+
+export default {
+    props: ['resources'],
+    components: {
+        LearningResource
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
